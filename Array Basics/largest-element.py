5e6f46065baf84ba432091f96463334546adc67c
Ashish Ranjan <ashishranjan@Ashishs-MacBook-Pro.local>
2026-02-18 15:08:16 +0530

    finding smallest and second smallest element in an array

--- Array Basics/largest-element.py
+++ Array Basics/largest-element.py
@@ -6,8 +6,16 @@ class OperationsOnArray:
                 maxEle = ele
 
         return maxEle
+    
+    def minElement(self, arr):
+        minEle = float('inf')
+        for ele in arr:
+            if ele < minEle:
+                minEle = ele
+        
+        return minEle
 
-    def SecondLargestElement(self, arr):
+    def secondLargestElement(self, arr):
         maxEle = arr[0]
         secondMaxEle = arr[0]
 
@@ -19,6 +27,41 @@ class OperationsOnArray:
                 secondMaxEle = ele
 
         return secondMaxEle
+    
+    def secondSmallestElement(self, arr):
+        minEle = float('inf')
+        secondMinEle = float('inf')
+
+        for ele in arr:
+            if ele<minEle:
+                secondMinEle = minEle
+                minEle = ele
+            elif ele<secondMinEle:
+                secondMinEle = ele
+        
+        return secondMinEle
+    
+    def thirdLargestElement(self, arr):
+        maxEle = arr[0]
+        secondMaxEle = arr[0]
+        thirdMaxEle = arr[0]
+
+        for ele in arr[1:]:
+            if ele>maxEle:
+                thirdMaxEle = secondMaxEle
+                secondMaxEle = maxEle
+                maxEle = ele
+            elif ele>secondMaxEle:
+                thirdMaxEle = secondMaxEle
+                secondMaxEle = ele
+            elif ele>thirdMaxEle:
+                thirdMaxEle = ele
+        
+        return thirdMaxEle
+
+
+    
+
                 
 
 
@@ -26,4 +69,7 @@ class OperationsOnArray:
 if __name__ == "__main__":
     arr = [2, 4, 1, 3, 5]
     print(OperationsOnArray().maxElement(arr))
-    print(OperationsOnArray().SecondLargestElement(arr))
+    print(OperationsOnArray().secondLargestElement(arr))
+    print(OperationsOnArray().thirdLargestElement(arr))
+    print(OperationsOnArray().minElement(arr))
+    print(OperationsOnArray().secondSmallestElement(arr))
