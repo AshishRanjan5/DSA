b75c0b14c6f062c7aeb283bc01083e12d107a630
Ashish Ranjan <ashishranjan@Ashishs-MacBook-Pro.local>
2026-02-19 10:26:12 +0530

    push zeroes at the end

---
+++ Array Basics/move-zeroes-at-end.py
@@ -0,0 +1,6 @@
+class MoveZerosAtEnd:
+    def function_BruteForce(self, arr):
+        pass
+
+if __name__ == "__main__":
+    arr = [2, 0, 0, 4, 5, 0, 1, 0, 3, 0]
---
+++ Array Basics/rotate-array.py
@@ -0,0 +1,86 @@
+class Rotate:
+    
+    def reverse(self, arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+
+    def rotateLeftOne(self, arr):
+        return arr[1:] + arr[:1]
+    
+    def rotateLeftOne_2(self, arr):
+        N = len(arr)
+        firstEle = arr[0]
+        for i in range(1, N):
+            arr[i-1] = arr[i]
+        arr[N-1] = firstEle
+
+    def rotateLeftK_Times(self, arr, K):
+        N = len(arr)
+
+        if N == 0:
+            return
+        
+        K = K%N
+
+        if K == 0:
+            return
+
+        self.reverse(arr, 0, K-1)
+        self.reverse(arr, K, N-1)
+        self.reverse(arr, 0, N-1)
+
+    def rotateRightOne(self, arr):
+        N= len(arr)
+        return arr[N-1:] + arr[:N-1]
+    
+    def rotateRightOne_2(self, arr):
+        N = len(arr)
+        lastEle = arr[N-1]
+
+        for i in range(N-2, -1, -1):
+            arr[i+1] = arr[i]
+        
+        arr[0] = lastEle
+
+    def rotateRightK_Times(self, arr, K):
+        N = len(arr)
+        K = K%N
+
+        if K == 0:
+            return
+        
+        if N == 0:
+            return
+        
+        self.reverse(arr, 0, N-1)
+        self.reverse(arr, K, N-1)
+        self.reverse(arr, 0, K-1)
+
+
+if __name__ == "__main__":
+    arr = [2, 4, 5, 1, 3]
+    print(Rotate().rotateLeftOne(arr)) # This does not modify the actual array
+    
+    arr = [2, 4, 5, 1, 3]
+    Rotate().rotateLeftOne_2(arr) # modifies the actual array
+    print(arr)
+
+    arr = [2, 4, 5, 1, 3]
+    Rotate().rotateLeftK_Times(arr, 1) # modifies the actual array
+    print(arr)
+
+    arr = [2, 4, 5, 1, 3]
+    print(Rotate().rotateRightOne(arr)) # This does not modify the actual array
+
+    arr = [2, 4, 5, 1, 3]
+    Rotate().rotateRightOne_2(arr) # modifies the actual array
+    print(arr)
+
+    arr = [2, 4, 5, 1, 3]
+    Rotate().rotateRightK_Times(arr, 1) # modifies the actual array
+    print(arr)
+
+    
+
